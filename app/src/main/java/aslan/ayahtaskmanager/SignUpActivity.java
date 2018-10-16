16cc34bf815a68a847b77d4ec7eb9b11b4a0573a
Adam Awad <adam.awad.2000@hotmail.com>
2018-10-16 13:21:34 +0300

    Login & SignUp

--- app/src/main/java/aslan/ayahtaskmanager/SignUpActivity.java
+++ app/src/main/java/aslan/ayahtaskmanager/SignUpActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignUpActivity extends AppCompatActivity {
 
     private EditText first;
@@ -14,6 +17,11 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText email2;
     private EditText password2;
     private Button save;
+
+    FirebaseUser user;
+    // alka2en elle ho bbne (user firebase) f2a jahzi
+    FirebaseAuth auth;
+    // alka2en bsa3dne a3ml sign up w sign in
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,10 @@ public class SignUpActivity extends AppCompatActivity {
         email2 = (EditText) findViewById(R.id.email2);
         password2 = (EditText)findViewById(R.id.password2);
         save = (Button)findViewById(R.id.save);
+
+        auth=FirebaseAuth.getInstance();
+        user= auth.getCurrentUser();
+        // btrj3 al user eza 3amil sign in(aluser al7ali)  aw brj3 null
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
