0dc64cd0dc056f7b284d29d561719a1afd8d308b
Adam Awad <adam.awad.2000@hotmail.com>
2018-11-06 13:20:46 +0200

    datahandler

--- app/src/main/java/aslan/ayahtaskmanager/LoginActivity.java
+++ app/src/main/java/aslan/ayahtaskmanager/LoginActivity.java
@@ -1,11 +1,19 @@
 package aslan.ayahtaskmanager;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,6 +21,9 @@ public class LoginActivity extends AppCompatActivity {
     private EditText password;
     private Button signup;
     private Button signin;
+
+    FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,39 +43,47 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(i2);
             }
         });
+    }
 
-//        private void dataHandler()
-//        {
-//            boolean isOk = true; // if all the fields filled well
-//            String email1 = email.getText().toString();
-//            String password1 = password.getText().toString();
-//
-//            if (email1.length()<4 || email1.indexOf('@')<0 || email1.indexOf('.')<0)
-//            {
-//                email.setError("Wrong Email");
-//                isOk = false; }
-//
-//            if (password1.length() < 8)
-//            {
-//                password.setError("Have to be at least 8 char");
-//                isOk = false; }
-//
-//            if (isOk)
-//                creatAcount(email1,password1);
-//            }
+    private void dataHandler() {
+        boolean isOk = true; // if all the fields filled well
+        String email1 = email.getText().toString();
+        String password1 = password.getText().toString();
 
+        if (email1.length() < 4 || email1.indexOf('@') < 0 || email1.indexOf('.') < 0) {
+            email.setError("Wrong Email");
+            isOk = false;
         }
 
-//        signin.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        Intent i1 = new Intent(LoginActivity.this, MainTabsActivity.class);
-//                        startActivity(i1);
-//                    }
-//                });
-//            }
-
+        if (password1.length() < 8) {
+            password.setError("wrong password");
+            isOk = false;
         }
+
+        if (isOk)
+            signIn(email1, password1);
+    }
+
+    private void signIn(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "signIn Successful.", Toast.LENGTH_SHORT).show();
+                 Intent intent=new Intent(LoginActivity.this,MainTabsActivity.class);
+                   startActivity(intent);
+                  finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "signIn failed." + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
+        });
+
+    }
+}
+
+
 
 
 
